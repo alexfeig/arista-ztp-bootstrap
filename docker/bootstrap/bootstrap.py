1d5c8b9512c47179e8c8384f184555a3188a1e0f
Alex Feigenson <alex.feigenson@gmail.com>
2017-03-06 20:20:59 -0600

    This is a hack

--- docker/bootstrap/bootstrap.py
+++ docker/bootstrap/bootstrap.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 
-"""This script is copied to a switch and executed. It does things"""
+import urllib2
+import json
+from subprocess import call
 
-import logging
-import time
+f = open('/sys/class/net/ma1/address','r')
+mac = f.read()
 
+url = 'http://ipam.home.lab/api/dcim/devices/?mac_address='
 
-def main():
-    while True:
-        print "This prints once a minute."
-        time.sleep(60)
+url_assembled = url + mac
 
+f.close()
 
-if __name__ == "__main__":
-    main()
+response = urllib2.urlopen(url_assembled)
+jsonresponse = json.load(response)
+
+device_name = jsonresponse[0]['name']
+device_ip = jsonresponse[0]['primary_ip']['address']
+
+config = ""
+config = config + "enable\n"
+config = config + "configure\n"
+config = config + "hostname " + device_name
+config = config + "\ninterface Management1\n"
+config = config + "ip address " + device_ip
+
+call("echo \"" + config + "\" | " + "/usr/bin/FastCli",shell=True)
